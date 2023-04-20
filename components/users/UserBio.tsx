9267962ba046a116827f944555216a79ceb67871
Matheus Ferreira <matheus.fer33@gmail.com>
2023-04-19 21:57:20 -0300

    added the ability to edit the user's profile bio

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -1,10 +1,13 @@
 import React, { useMemo } from "react";
 import { format } from "date-fns";
 
+import { BiCalendar } from "react-icons/bi";
+
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useUser from "@/hooks/useUser";
+import useEditModal from "@/hooks/useEditModal";
+
 import Button from "../Button";
-import { BiCalendar } from "react-icons/bi";
 
 interface UserBioProps {
   userId: string;
@@ -13,6 +16,8 @@ interface UserBioProps {
 const UserBio: React.FC<UserBioProps> = ({ userId }) => {
   const { data: currentUser } = useCurrentUser();
   const { data: fetchedUser } = useUser(userId);
+
+  const editModal = useEditModal();
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
@@ -26,7 +31,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? (
-          <Button secondary label="Edit" onClick={() => {}} />
+          <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
           <Button onClick={() => {}} label="Follow" secondary />
         )}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
-import { Toaster } from "react-hot-toast";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 
 import Layout from "@/components/Layout";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
+import EditModal from "@/components/modals/EditModal";
+
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -12,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <SessionProvider session={pageProps.session}>
       <Toaster />
       <RegisterModal />
+      <EditModal />
       <LoginModal />
       <Layout>
         <Component {...pageProps} />
