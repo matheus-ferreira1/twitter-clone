2ea5360e2d4cf2988f36ee188487df780197b71b
Matheus Ferreira <matheus.fer33@gmail.com>
2023-04-24 23:19:06 -0300

    added the head metatag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,18 @@ import RegisterModal from "@/components/modals/RegisterModal";
 import EditModal from "@/components/modals/EditModal";
 
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
+      <Head>
+        <title>MF - twitter</title>
+        <meta
+          name="description"
+          content="Twitter clone for studying purposes"
+        />
+      </Head>
       <Toaster />
       <RegisterModal />
       <EditModal />
